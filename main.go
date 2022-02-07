e282186ce5f0ea2d3295c009e33923e435c6f30d
Amirreza Kafshnochi <amirrezanochi@gmail.com>
2022-02-07 18:32:51 +0330

    ep05

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// int int8  int16 int32 int64
-	// float float32 float64
-	// string
-	// bool
+	// + - * / %
+	// == != && || ()
 
-	// var myFirstVar int = 20
-	myFirstVar := false
+	myFirstVar, mySecondVar := 5, 2
+	myThirdVar := "hello"
 
-	fmt.Println(myFirstVar)
+	result := (myFirstVar != mySecondVar) || (myThirdVar == "goodbye")
+
+	fmt.Println(result)
 }
