a3dd894984ad9c72fc46db2ae80dd9cb1b259f90
Amirreza Kafshnochi <amirrezanochi@gmail.com>
2022-02-07 18:21:25 +0330

    ep04

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int int8  int16 int32 int64
+	// float float32 float64
+	// string
+	// bool
+
+	// var myFirstVar int = 20
+	myFirstVar := false
+
+	fmt.Println(myFirstVar)
+}
