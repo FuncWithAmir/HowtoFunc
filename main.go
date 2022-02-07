dd771ca59f04664c0705f5f34a7b51223f2ff49a
Amirreza Kafshnochi <amirrezanochi@gmail.com>
2022-02-07 22:42:37 +0330

    - Added missing operators

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import "fmt"
 
 func main() {
 	// + - * / %
-	// == != && || ()
+	// < > == <= >= !=
+	// () && ||
 
 	myFirstVar, mySecondVar := 5, 2
 	myThirdVar := "hello"
 
-	result := (myFirstVar != mySecondVar) || (myThirdVar == "goodbye")
+	result := (myFirstVar <= mySecondVar) || (myThirdVar == "goodbye")
 
 	fmt.Println(result)
 }
